4b5763e8694dacbb598c31d91649a0d8c6a8a24e
BayuSuryadinata <bayusurya380@gmail.com>
2024-10-24 14:27:58 +0700

    a

--- app.js
+++ app.js
@@ -14,16 +14,17 @@
 
 // JANGAN HAPUS / UBAH SCRIPT DI BAWAH
 const calculateKomisi = (nilaiKontrak) => {
-  if (nilaiKontrak >= 1000000){
-    return nilaiKontrak * 0 + 100000
-  }else if (nilaiKontrak >= 5000000){
+  if (nilaiKontrak >= 5000000){
     return nilaiKontrak * 0.05
+
+  }else if (nilaiKontrak >= 1000000){
+    return nilaiKontrak * 0 + 100000
   }else{
     return 0
 };
 
 };
-console.log(calculateKomisi(2000000))
+console.log(calculateKomisi())
 
 
 module.exports = calculateKomisi
